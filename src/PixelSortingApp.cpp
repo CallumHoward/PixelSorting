8f0e6499b4d57c75325e0c5a70a15c2237ce2ab4
CallumHoward <callumh@cse.unsw.edu.au>
2019-06-18 07:43:35 +1000

    xcode files

---
+++ src/PixelSortingApp.cpp
@@ -0,0 +1,34 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class PixelSortingApp : public App {
+  public:
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+	void update() override;
+	void draw() override;
+};
+
+void PixelSortingApp::setup()
+{
+}
+
+void PixelSortingApp::mouseDown( MouseEvent event )
+{
+}
+
+void PixelSortingApp::update()
+{
+}
+
+void PixelSortingApp::draw()
+{
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+CINDER_APP( PixelSortingApp, RendererGl )
